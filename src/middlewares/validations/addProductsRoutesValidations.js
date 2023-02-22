358118e0342e6c2016b9c3b5372161bc95075fbd
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-22 16:48:16 -0300

    Requisite 4 done

--- src/middlewares/validations/addProductsRoutesValidations.js
+++ src/middlewares/validations/addProductsRoutesValidations.js
@@ -1,9 +1,13 @@
 const addProductsRoutesValidations = (req, res, next) => {
   const { name } = req.body;
   if (!name) {
- res.status(401)
-  .send('You have to inform the product name'); 
+ res.status(400)
+  .send({ message: '"name" is required' }); 
 }
+  if (name.length < 5) {
+    res.status(422)
+.send({ message: '"name" length must be at least 5 characters long' });
+  }
   next();
 };
 
