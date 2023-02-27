2a8498b263e5e13a6f05f9c6052eb20d05ebcf6e
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-26 22:46:25 -0300

    mock improved

--- tests/unit/mocks/productsMock.js
+++ tests/unit/mocks/productsMock.js
@@ -14,4 +14,4 @@ const db =
     }
   ];
 
-module.exports = { db };
+module.exports = db ;
--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const chai = require('chai');
 const sinon = require('sinon');
-const { db } = require('../mocks/productsMock');
+const db = require('../mocks/productsMock');
 const connection = require('../../../src/connection');
 const chaiHTTP = require('chai-http');
 const {productsModel} = require('../../../src/models')
@@ -14,11 +14,19 @@ describe('Products route tests', function() {
     afterEach(function() {
       sinon.restore();
       });
-    it('should return all products', async function(){
+
+    beforeEach(async() =>{
       sinon.stub(connection, 'execute').resolves([db]);
+    });
+    it('should return all products', async function(){
       const expectedDB = await productsModel.getAllProducts();
       expect(expectedDB).to.be.deep.equal(db);
     });
+
+    /* it('should return product by Id', async function(id){
+      const expectedDB = await productsModel.getProductById(id);
+      expect(expectedDB).to.be.deep.equal(db[0])
+    }) */
   
 });
 });
