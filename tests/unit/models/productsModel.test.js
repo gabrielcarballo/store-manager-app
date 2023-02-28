2848247d558695c97aa7a54c30a7e06e720de389
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-28 18:56:51 -0300

    Starting productsServices tests

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -17,24 +17,24 @@ describe('Products route tests', function() {
       sinon.restore();
       });
 
-    beforeEach(async() =>{
-      sinon.stub(connection, 'execute').resolves([getAllProductsMock]);
-    });
     it('should return all products', async function(){
+      sinon.stub(connection, 'execute').resolves([getAllProductsMock]);
       const expectedDB = await productsModel.getAllProducts();
       expect(expectedDB).to.be.deep.equal(getAllProductsMock);
     });
 
     it('should return product by Id', async function(){
+      sinon.stub(connection, 'execute').resolves([getAllProductsMock]);
       const expectedDB = await productsModel.getProductById(1);
       expect(expectedDB).to.be.deep.equal(getAllProductsMock[0]);
     });
 
     it('should return the all products with added one', async function(){
+      sinon.stub(connection, 'execute').resolves([addProductMock]);
       const productToAdd = {"name": "Laço da Verdade"};
       await productsModel.addProduct(productToAdd);
-      addProductMock.push(productToAdd);
-      expect(addProductMock).to.be.deep.equal(addProductMock);
+      const expectedDB = await productsModel.getAllProducts();
+      expect(expectedDB).to.be.deep.equal(addProductMock);
     });
   
 });
